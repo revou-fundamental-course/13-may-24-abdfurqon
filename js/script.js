2908ba69df241740dec0298667f4a0c6868c27a2
abdfurqon <kokofurqon@gmail.com>
2024-05-23 23:33:20 +0700

    update-last

--- js/script.js
+++ js/script.js
@@ -1,30 +1,56 @@
-// ini js
-
-let isKelilingVisible = false; //Keliling is Hide
+// The javascript
+let isKelilingVisible = false;
 function hideOrShow(){
     if (isKelilingVisible == false) {
         isKelilingVisible = true;
         document.getElementById("luas-section").style.display = "none";
         document.getElementById("keliling-section").style.display = "block";
+        document.querySelector("button").innerHTML = "Switch ke Luas Segitiga";
     } else {
         isKelilingVisible = false;
         document.getElementById("luas-section").style.display = "block";
         document.getElementById("keliling-section").style.display = "none";
+        document.querySelector("button").innerHTML = "Switch ke Keliling Segitiga";
     }
-    
 }
 
 function validation() {
     let alasValue = document.getElementById("alas-input").value;
     let tinggiValue = document.getElementById("tinggi-input").value;
-    // console.log(alasValue);
-    // console.log(tinggiValue);
+    document.getElementById("show-alas").innerHTML = alasValue;
+    document.getElementById("show-tinggi").innerHTML = tinggiValue;
 
     if (alasValue != '' && tinggiValue != '') {
-        // console.log('sukses validasi');
         let hasil = 0.5 * alasValue * tinggiValue;
-        console.log(hasil)
+        document.getElementById("show-result").style.display = "block";
+        document.getElementById("show-hasil").innerHTML = hasil;
     } else {
-        alert('Data belum terisi, silakan masukkan angka')
+        alert('Input tidak valid, silakan masukkan angka')
     }
+}
+
+function resetValue() {
+    document.getElementById("show-result").style.display = "none";
+}
+
+function hitungKeliling() {
+    let sisiAValue = document.getElementById("sisi-a").value;
+    let sisiBValue = document.getElementById("sisi-b").value;
+    let sisiCValue = document.getElementById("sisi-c").value;
+    
+    document.getElementById("show-sisi-a").innerHTML = sisiAValue;
+    document.getElementById("show-sisi-b").innerHTML = sisiBValue;
+    document.getElementById("show-sisi-c").innerHTML = sisiCValue;
+
+    if (sisiAValue != '' && sisiBValue != '' && sisiCValue != '') {
+        let hasilKeliling = parseFloat(sisiAValue)+ parseFloat(sisiBValue) + parseFloat(sisiCValue);
+        document.getElementById("show-result-keliling").style.display = "block";
+        document.getElementById("show-hasil-keliling").innerHTML = hasilKeliling;
+    } else {
+        alert('Input tidak valid, silakan masukkan angka')
+    }
+}
+
+function resetValueKeliling() {
+    document.getElementById("show-result-keliling").style.display = "none";
 }
